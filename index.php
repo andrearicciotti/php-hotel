6a39525387c9cbb8b4df65d73b152466bbc1fcbc
andrearicciotti <ricciotti.andrea1993@gmail.com>
2023-12-12 17:12:22 +0100

    add: hotels array and output datas in browser

---
+++ index.php
@@ -0,0 +1,68 @@
+<?php
+
+$hotels = [
+
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+
+];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Hotel</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div>
+        <?php foreach($hotels as $hotel) { ?>
+        <h2>Hotel: <?php echo $hotel['name'] ?></h2>
+        <p>Description: <?php echo $hotel['description'] ?></p>
+        <p>Parking: <?php if($hotel['parking'] === true) {echo 'Yes';} else {echo 'No';} ?></p>
+        <p>Vote: <?php echo $hotel['vote'] ?> of 5</p>
+        <p>Distance to center: <?php echo $hotel['distance_to_center'] ?> Km.</p>
+        <?php } ?>
+    </div>
+</body>
+
+</html>
